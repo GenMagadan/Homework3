d824983701a5bd7b41984e04b388db7585b5a1e9
Gennady <genchik111@yandex.ru>
2023-04-01 13:09:14 +0300

    Add task19_1

---
+++ task19_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Definition(number));
+
+string Definition(int number)
+{
+   if (number > 9999 && number < 99999)
+   {
+      if (number / 10000 == number % 10 || (number % 10000) / 1000 == (number % 100) / 10)
+      {
+         return $"Число {number} является палидромом";
+      }
+      return $"Число {number} не является палидромом";
+   }
+   return $"Число {number} не является пятизначным числом";
+}
+
