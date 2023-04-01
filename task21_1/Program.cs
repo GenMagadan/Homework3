d5a6d71f58c8ee57b97d856fce72d46952f8bbd2
Gennady <genchik111@yandex.ru>
2023-04-01 14:57:30 +0300

    Add task21_1

---
+++ task21_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите координаты первой точки: ");
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки: ");
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+int z2 = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine(Length(x1, y1, z1, x2, y2, z2));
+double Length(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+   double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+   return Math.Round(result, 2);
+}
+
