0b2bba51956102e923bfbd81789b2211ad6b4336
Gennady <genchik111@yandex.ru>
2023-04-01 14:02:03 +0300

    Add task 19_2

---
+++ task19_2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Актуально для слова, состоящего из разных символов. Для строки не подходит.*/
+
+Console.Write("Введите слово: ");
+string str = Console.ReadLine();
+string newstr = str.ToUpper();
+
+Console.WriteLine(Definition(newstr));
+
+string Definition(string newstr)
+{
+   for (int i = 0; i < newstr.Length / 2; i++)
+   {
+      if (newstr[i] != newstr[newstr.Length - i - 1])
+      {
+         return "Не является палидромом";
+      }
+   }
+   return "Является палидромом";
+}
+
+/* 45Ghjjhg54 - является палидромом */
