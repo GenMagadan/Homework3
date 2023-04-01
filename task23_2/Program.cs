b2f42a47acd97c55298b82551abb432d316e0cf2
Gennady <genchik111@yandex.ru>
2023-04-01 15:36:09 +0300

    Add task23_2

---
+++ task23_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+
+CubeTable(N);
+
+void CubeTable(int N)
+{
+   int[] arr = new int[N + 1];
+   for (int i = 1; i < arr.Length; i++)
+   {
+      arr[i] = i;
+      Console.WriteLine(Math.Pow(i, 3));
+   }
+}
